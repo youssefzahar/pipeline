a820455fd7383e14760d69ab16c703850e8a4831
youssefzahar <61435160+youssefzahar@users.noreply.github.com>
2023-11-16 14:03:58 +0100

    replace front

--- src/main/java/tn/esprit/devops_project/repositories/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.devops_project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.devops_project.entities.Supplier;
-
-public interface SupplierRepository extends JpaRepository<Supplier, Long> {
-
-}
